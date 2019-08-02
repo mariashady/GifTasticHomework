f72ce362ec2832ee1678046b13e02625d4897edf
Maria Shady <maria.shady@yahoo.com>
2019-08-02 16:44:56 -0400

    padding around buttons

--- assets/javascript/homework6.js
+++ assets/javascript/homework6.js
@@ -3,6 +3,7 @@ let userChoice = $(".userInput").val();
 
 function makeButton() {
     $(".newButtons").append(`<button class="addedButtons">${$(".userInput").val()}</button>`);
+    $(".userInput").val("");
 }
 $("#submitButton").on("click", function(event){
     event.preventDefault();
