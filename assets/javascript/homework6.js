ab0ae826da4376246abf21780f5f37517b4e29f1
Maria Shady <maria.shady@yahoo.com>
2019-08-04 10:27:11 -0400

    trying new approach

--- assets/javascript/homework6.js
+++ assets/javascript/homework6.js
@@ -1,12 +1,41 @@
+let food = ["pizza", "tacos", "burgers"];
 
-let userChoice = $(".userInput").val();
+function makeButtons() {
 
-function makeButton() {
-    $(".newButtons").append(`<button class="addedButtons">${$(".userInput").val()}</button>`);
-    $(".userInput").val("");
+    $("#buttonsView").empty();
+
+    for (let i = 0; i < foods.length; i++){
+        let a = $("<button>")
+        a.addClass("food");
+        a.attr("data-name", food[i]);
+        a.text(food[i]);
+        $("#buttonsView").append(a);
+    }
 }
-$("#submitButton").on("click", function(event){
-    event.preventDefault();
-    makeButton();
-});
+
+// let userChoice = $(".userInput").val();
+
+// function makeButton() {
+//     $(".newButtons").append(`<button class="addedButtons">${$(".userInput").val()}</button>`);
+//     $(".userInput").val("");
+// }
+// $("#submitButton").on("click", function(event){
+//     event.preventDefault();
+//     makeButton();
+// });
+
+// function displayFood() {
+
+//     let food = $(this).attr("foodName");
+//     let queryURL = "http://api.giphy.com/v1/gifs/search?api_key=eCMi4M1b7vas4I9GdRCg4WxBZreqr6Mm&q=" + food + "&limit=5&rating=g";
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//       $("#gifsHere").push(response);
+//     });
+// }
+
+
 
