9ed71643ee85958eda92e7aa1f4084f2745421bd
Maria Shady <maria.shady@yahoo.com>
2019-08-04 15:44:37 -0400

    html adjustments

--- assets/javascript/homework6.js
+++ assets/javascript/homework6.js
@@ -20,6 +20,21 @@ function makeButtons() {
 //         food.push(userFood);
 //         return false;
 //     }
+// function displayGifs(){
+//     let food = $(this).attr("data-name")
+//     let queryURL = "http://api.giphy.com/v1/gifs/search?api_key=eCMi4M1b7vas4I9GdRCg4WxBZreqr6Mm&q=" + food + "&limit=5&rating=g";
+
+//         $.ajax({url: queryURL, method:"GET"}).done(function (response) {
+//             console.log(response.data);
+//             let results = response.data;
+//             for (let i = 0; i < results.length; i++ ) {
+//                 let gifDiv = $("<div class=gifs>");
+//                 let showGif = $("<img>");
+//                     showGif.attr("src", results[i].images.fixed_height_still.url);
+                    
+//             }
+//         }
+// }
     
 
 makeButtons();
