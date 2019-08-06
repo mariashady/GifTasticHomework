c478a9747785c5c2f354f38e5829372563a70f54
Maria Shady <maria.shady@yahoo.com>
2019-08-05 23:03:15 -0400

    gifs appear

--- assets/javascript/homework6.js
+++ assets/javascript/homework6.js
@@ -1,72 +1,72 @@
+$(document).ready(function(){
+    let food = ["pizza", "tacos", "burgers"];
 
-let food = ["pizza", "tacos", "burgers"];
-
-function makeButtons() {
-
-    $("#buttonsView").empty();
-
-    for (let i = 0; i < food.length; i++){
-        let a = $("<button>")
-        a.addClass("food");
-        a.attr("data-name", food[i]);
-        a.text(food[i]);
-        $("#buttonsView").append(a);
+    function makeButtons() {
+    
+       $("#buttonsView").empty();
+    
+       for (let i = 0; i < food.length; i++){
+           let a = $("<button>")
+           .addClass("food topic")
+           .attr("data-name", food[i])
+           .text(food[i]);
+           $("#buttonsView").append(a);
+       }
     }
-}
     
-// $(".newButtons").on("click", "button.food", function(){
+    
+    function displayGifs(){
+       let apiKey = "eCMi4M1b7vas4I9GdRCg4WxBZreqr6Mm"
+       let food = $(this).attr("data-name")
+       let queryURL = "http://api.giphy.com/v1/gifs/search?";
+       let queryParam = {
+           "api_key": apiKey,
+           "q": food,
+           "limit": 10
+       };
+       let completeURL = queryURL + $.param(queryParam)
+           $.ajax({
+               url: completeURL,
+               method:"GET"
+           }).then(function (response) {
+               $("#gifsHere").empty();
+               console.log(response.data);
+               let results = response.data;
+               for (let i = 0; i < results.length; i++ ) {
+                   let gifDiv = $("<div class=gifs>"); 
+                   let showGif = $("<img>");
+                       showGif.attr("src", results[i].images.fixed_height_still.url);
+                       $("#gifsHere").append(showGif);
+                   
+
+    
+               }
+           });
+    };
+    
+    makeButtons();
+    
+    
+    function makeButton() {
+        $(".newButtons").append(`<button class="addedButtons">${$(".userInput").val()}</button>`);
+        $(".userInput").val("");
+    }
+    $("#submitButton").on("click", function(){
+        event.preventDefault();
         
-//         let userFood = $("#foodInput").val().trim();
-//         food.push(userFood);
-//         return false;
-//     }
-// function displayGifs(){
-//     let food = $(this).attr("data-name")
-//     let queryURL = "http://api.giphy.com/v1/gifs/search?api_key=eCMi4M1b7vas4I9GdRCg4WxBZreqr6Mm&q=" + food + "&limit=5&rating=g";
-
-//         $.ajax({url: queryURL, method:"GET"}).done(function (response) {
-//             console.log(response.data);
-//             let results = response.data;
-//             for (let i = 0; i < results.length; i++ ) {
-//                 let gifDiv = $("<div class=gifs>");
-//                 let showGif = $("<img>");
-//                     showGif.attr("src", results[i].images.fixed_height_still.url);
-                    
-//             }
-//         }
-// }
+        let userChoice = $(".userInput").val();
+        food.push(userChoice);
+       makeButton();
+    });
+    $(document).on("click", ".topic", displayGifs);
+    
     
 
-makeButtons();
+
+    
 
 
 
 
 
-
-// let userChoice = $(".userInput").val();
-
-// function makeButton() {
-//     $(".newButtons").append(`<button class="addedButtons">${$(".userInput").val()}</button>`);
-//     $(".userInput").val("");
-// }
-// $("#submitButton").on("click", function(event){
-//     event.preventDefault();
-//     makeButton();
-// });
-
-// function displayFood() {
-
-//     let food = $(this).attr("foodName");
-//     let queryURL = "http://api.giphy.com/v1/gifs/search?api_key=eCMi4M1b7vas4I9GdRCg4WxBZreqr6Mm&q=" + food + "&limit=5&rating=g";
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function(response) {
-//       $("#gifsHere").push(response);
-//     });
-// }
-
-
-
+});
